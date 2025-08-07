9a5958042c5119e76a04f8d292a1127a30f72b2e
lunglinhlaplanh <158856626+hungdev1602@users.noreply.github.com>
2025-08-08 01:15:11 +0300

    add PUG

--- index.ts
+++ index.ts
@@ -3,8 +3,14 @@ import express, { Express, Request, Response } from "express"
 const app: Express = express()
 const port: number = 3000
 
+// Views
+app.set("views", `${__dirname}/views`); // Tìm đến thư mục tên là views
+app.set("view engine", "pug") // template engine sử dụng: pug
+// End Views
+
+
 app.get("/topics", (req: Request, res: Response) => {
-  res.send("Hello world")
+  res.render("client/pages/topics/index")
 })
 
 app.listen(port, () => {
