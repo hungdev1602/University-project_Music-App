beef9b48e5898da83373498ccc311264de93b4db
lunglinhlaplanh <158856626+hungdev1602@users.noreply.github.com>
2025-08-08 14:05:54 +0300

    controller + routes

--- index.ts
+++ index.ts
@@ -2,8 +2,7 @@ import express, { Express, Request, Response } from "express"
 // ENV
 import dotenv from "dotenv"
 dotenv.config()
-
-import { Topic } from "./models/client/topic.model"
+import { routesClient } from "./routes/client/index.route"
 
 const app: Express = express()
 const port: number = 3000
@@ -17,16 +16,8 @@ app.set("views", `${__dirname}/views`); // Tìm đến thư mục tên là views
 app.set("view engine", "pug") // template engine sử dụng: pug
 // End Views
 
-
-app.get("/topics", async (req: Request, res: Response) => {
-  const topics = await Topic.find({
-    deleted: false
-  })
-
-  console.log(topics)
-
-  res.render("client/pages/topics/index")
-})
+//route client
+routesClient(app)
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
