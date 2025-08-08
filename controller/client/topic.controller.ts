8c7852cd1ac50bf2f3e104b9fb12823337fde45e
lunglinhlaplanh <158856626+hungdev1602@users.noreply.github.com>
2025-08-09 00:10:33 +0300

    Làm trang Chủ đề bài hát (bên client)

--- controller/client/topic.controller.ts
+++ controller/client/topic.controller.ts
@@ -6,7 +6,10 @@ export const index = (async (req: Request, res: Response) => {
     deleted: false
   })
 
+  console.log(topics)
+
   res.render("client/pages/topics/index", {
-    pageTitle: "Topics song"
+    pageTitle: "Song topics",
+    topics: topics
   })
 })
