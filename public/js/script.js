65454d24ac964a80f29792368ef22f5b7eaf9a78
lunglinhlaplanh <158856626+hungdev1602@users.noreply.github.com>
2025-08-12 23:51:31 +0300

    tạm dừng ảnh khi nhạc dừng, ảnh xoay khi nhạc chạy

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,24 @@
-const ap = document.querySelector("#aplayer")
-if(ap){
-  const dataSong = JSON.parse(ap.getAttribute("data-song"))
-  const singerName = ap.getAttribute("data-singer")
+const aPlayer = document.querySelector("#aplayer")
+if(aPlayer){
+  const dataSong = JSON.parse(aPlayer.getAttribute("data-song"))
+  const singerName = aPlayer.getAttribute("data-singer")
+  const rollingAvatar = document.querySelector(".inner-play .inner-avatar")
 
-  new APlayer({
-    container: ap,
+  const ap = new APlayer({
+    container: aPlayer,
     audio: [{
         name: dataSong.title,
         artist: singerName,
         url: dataSong.audio,
         cover: dataSong.avatar
     }]
-});
+  });
+
+  ap.on("play", () => {
+    rollingAvatar.style.animationPlayState = "running"
+  })
+
+  ap.on("pause", () => {
+    rollingAvatar.style.animationPlayState = "paused"
+  })
 }
